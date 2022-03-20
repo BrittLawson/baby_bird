f7a1fbcae3d2f9eea6742bdb26e7506352a01905
Britt Lawson <brittlawsonrn@gmail.com>
2022-03-20 12:21:55 -0400

    updated sql file with jump, canopy, dropzone tables

--- skydiveLogbook.sql
+++ skydiveLogbook.sql
@@ -1,23 +1,45 @@
---Skydiver: id, USPA member #, USPA license, license type, # jumps (? or maybe just method to get in code)
---Logbook: jump #, DZ id, canopy id, aircraft, freefall time(?), exit altitude, deploy altitude, landing distance from target, maneuver, description
---DZ: id, name, USPA?
---address: id, DZ id, city, state, country
---Canopy: id, model, size
-
 DROP TABLE IF EXISTS jump;
+DROP TABLE IF EXISTS canopy;
+DROP TABLE IF EXISTS dropzone;
+
+
+CREATE TABLE dropzone
+(
+	dz_id SERIAL,
+	dz_name VARCHAR(200) NOT NULL,
+	city_state VARCHAR(100) NOT NULL,
+	country VARCHAR(100) DEFAULT 'USA',
+
+	CONSTRAINT pk_dz_id PRIMARY KEY (dz_id)
+);
+
+CREATE TABLE canopy
+(
+	canopy_id SERIAL,
+	platform VARCHAR(100) NOT NULL,
+	size_square_feet INTEGER NOT NULL,
+
+	CONSTRAINT pk_canopy_id PRIMARY KEY (canopy_id),
+	CONSTRAINT chk_positive_size CHECK (size_square_feet > 0)
+);
 
 CREATE TABLE jump
 (
 	jump_number INTEGER,
-	date DATE NOT NULL,
-	dropzone VARCHAR(200),
+	date DATE DEFAULT CURRENT_DATE,
+	dz_id INTEGER NOT NULL,
 	aircraft VARCHAR(100),
-	exit_altitude INTEGER,
-	deployment_altitude INTEGER,
-	canopy VARCHAR(100),
+	exit_altitude_in_feet INTEGER NOT NULL,
+	deployment_altitude_in_feet INTEGER NOT NULL,
+	canopy_id INTEGER NOT NULL,
+	distance_from_target_feet INTEGER,
 	maneuver VARCHAR(200),
 	description TEXT,
-	
+
 	CONSTRAINT pk_jump_number PRIMARY KEY (jump_number),
-	CONSTRAINT chk_appropriate_altitudes CHECK (deployment_altitude <= exit_altitude)
+	CONSTRAINT chk_valid_jump_number CHECK (jump_number > 0),
+	CONSTRAINT fk_dz_id FOREIGN KEY (dz_id) REFERENCES dropzone (dz_id),
+	CONSTRAINT fk_canopy_id FOREIGN KEY (canopy_id) REFERENCES canopy (canopy_id),
+	CONSTRAINT chk_appropriate_altitudes CHECK (deployment_altitude_in_feet <= exit_altitude_in_feet),
+	CONSTRAINT chk_appropriate_distance CHECK (distance_from_target_feet >= 0)
 );
